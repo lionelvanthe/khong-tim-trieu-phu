46cb32b6a74fcdf6cc94635ef10fb9946d85eb63
The <lionelvanthe@gmail.com>
2021-12-15 11:05:32 +0700

    linh tinh

--- app/src/main/java/com/example/khongtimtrieuphu/views/MainActivity.java
+++ app/src/main/java/com/example/khongtimtrieuphu/views/MainActivity.java
@@ -86,6 +86,7 @@ public class MainActivity extends AppCompatActivity implements OnActionCallBack 
         else if(fragment instanceof PlayFragment){
             ((PlayFragment) fragment).stopGame();
         }
+
         else{
             super.onBackPressed();
         }
